3d2bddf4158b6a92dd58e926cce33cafd6f7d7e8
Emmanuel Adebanjo <adebanjoemmanuelo1@gmail.com>
2021-08-27 00:38:04 +0100

    TextField clears onSubmitted

--- lib/Widget/userInputAndDisplayWidget.dart
+++ lib/Widget/userInputAndDisplayWidget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:task_2/Widget/displayItem.dart';
+import '../Widget/displayItem.dart';
 
 import '../constants.dart';
 
